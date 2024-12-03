f978c3b6558f229311593bc0596cdc2a3439c829
Kavin Abregana <abreganakavin@gmail.com>
2024-12-03 14:15:52 +0800

    init files

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "831401";
+  let InstanceId_Widget = "";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "848702";
+  let InstanceId_StarRating = "830865";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
